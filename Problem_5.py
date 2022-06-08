1c71a21e08dbf96ee2cc2f3de19cafbe6ac2a112
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-09 00:14:48 +0530

    program to perform all arithmetic operations .

---
+++ Problem_5.py
@@ -0,0 +1,7 @@
+# Write a program to perform all arithmetic operations .
+a,b=int(input("Enter two numbers: ")),int(input())
+print("Additon->"+str(a+b)+"")
+print("Subtraction->"+str(a-b)+"")
+print("Multiplication->"+str(a*b)+"")
+print("Division->"+str(a/b)+"")
+print("Modulus->"+str(a%b)+"")
