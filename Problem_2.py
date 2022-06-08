10763ac006faba05461b172d4fc9fede54a73cdd
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-08 23:41:35 +0530

    python program to print area of circle and rectangle

---
+++ Problem_2.py
@@ -0,0 +1,9 @@
+#Write a python program to print area of circle and rectangle
+# for Circle
+pi=3.14
+r=float(input("Enter the radius of circle: "))
+print("Area of circle is:",pi*r**2)
+
+#for python
+a,b=int(input("Enter sides of rectangle: ")),int(input())
+print("Area of rectangle:",a*b)
