f5b7c9be42ddeaa06dccef654aef487cce3ab3bf
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-08 23:50:48 +0530

    program to get name and age from user and print appropriate message

---
+++ Problem_3.py
@@ -0,0 +1,4 @@
+#Write a program to get name and age from user and print appropriate message
+name=str(input("Enter your name: "))
+age=(input("Enter your age: "))
+print("Hi "+name+"! Your age is "+age+".")
