8344acf9b5c132d5cb87dd3a1a13ef3a083451e8
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-10 23:18:47 +0530

    Program to find max among three numbers

---
+++ Problem_11.py
@@ -0,0 +1,5 @@
+#Python Program to find max among three numbers
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+n3=int(input("Enter third number:"))
+print("Max among inserted three nos is",max(n1,n2,n3))
