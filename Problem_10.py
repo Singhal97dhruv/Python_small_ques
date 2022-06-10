5d23bb8505040f8729bebc9ff8d87f716e4c973a
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-10 22:11:35 +0530

    program to take marks of 5 subjects and print avg,percentage and total marks.

---
+++ Problem_10.py
@@ -0,0 +1,10 @@
+# Python program to take marks of 5 subjects and print avg,percentage and total marks.
+sub1=int(input("Enter the marks for subject1: "))
+sub2=int(input("Enter the marks for subject1: "))
+sub3=int(input("Enter the marks for subject1: "))
+sub4=int(input("Enter the marks for subject1: "))
+sub5=int(input("Enter the marks for subject1: "))
+sum=sub1+sub2+sub3+sub4+sub5
+print("Total of five subjects is: ",sum)
+print("Average of five subjects is:",sum/500)
+print("Percentage of five subjects is:",sum*100/500)
