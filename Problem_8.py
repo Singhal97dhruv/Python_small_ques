0dab156116abf993c437c2eb408f8b5be6418e92
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-10 21:06:38 +0530

    program to calculate difference between two sets

---
+++ Problem_8.py
@@ -0,0 +1,11 @@
+#Python program to calculate difference between two sets
+x=set()
+y=set()
+n1=int(input("Enter the number of elements you want in set1: "))
+n2=int(input("Enter the number of elements you want in set2: "))
+for i in range(n1):
+    x.add(int(input()))
+for i in range(n2):
+    y.add(int(input()))
+print(x.difference(y))
+print(y-x)
