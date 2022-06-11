2f17750a6967bffa371f9a89066a83f8377e40e4
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-11 23:53:14 +0530

    Program that perform all compound operations

---
+++ Problem_16.py
@@ -0,0 +1,25 @@
+# Python program that perform all compound operations
+# -=,+=,*=,/=,%=
+
+num=5
+print("Given number is",num)
+
+# Addition assignment operator
+num+=4
+print("After addition assignment operator:",num)
+
+#Subtraction assignment operator
+num-=4
+print("After subtraction assignment operator:",num)
+
+#Multiplication assignment operator
+num*=4
+print("After multiplication assignment operator:",num)
+
+#Division assignment operator
+num/=4
+print("After division assignment operator:",num)
+
+#Division assignment operator
+num%=5
+print("After divisioin assignment operator:",num)
