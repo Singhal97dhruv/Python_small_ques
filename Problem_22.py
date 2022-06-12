407d890851091a44078f26dd50bc20790fdcdcaa
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-13 00:28:20 +0530

    problem to check entered char is whether vowel or consonant

---
+++ Problem_22.py
@@ -0,0 +1,7 @@
+#Python problem to check entered char is whether vowel or consonant
+
+c=input("Enter a character: ")
+if(c=='a' or c=='e' or(c=='i')or(c=='o')or(c=='u')):
+    print("Entered character is a vowel")
+else:
+    print("Enter character is a consonant")
