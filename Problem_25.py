75c0005dba5656f3fdb0de3446b2bfe8957918d2
Dhruv <Dhruvsinghal9876@gmail.com>
2022-06-13 00:51:48 +0530

    Problem-26

---
+++ Problem_25.py
@@ -0,0 +1,6 @@
+#python program to get student marks and if less than 40 it is fail otherwise pass
+marks=int(input("Enter the marks of student: "))
+if(marks<40):
+    print("Student failed")
+else:
+    print("Student passed")
---
+++ Problem_26.py
@@ -0,0 +1,7 @@
+#Python problem to check a number,even or odd
+n=int(input("Enter the number: "))
+if(n%2==0):
+    print("Number is even")
+else:
+    print("Number is odd")
+print("Square of the number is :",pow(n,2))
